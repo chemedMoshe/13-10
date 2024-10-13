d57b268c65a90b7d5e9249eafc8cd64552a9b742
Moshe <m0504136536@gmail.com>
2024-10-13 17:41:18 +0300

    create verify per role

--- src/middleware/verify.ts
+++ src/middleware/verify.ts
@@ -3,7 +3,19 @@ import jwt from "jsonwebtoken"
 import 'dotenv/config'
 import TokenPayloadDTO from "../types/modelDTO/tokenPayloadDTO"
 
-const verify = async (req:Request, res:Response, next:NextFunction):Promise<void> => {
+export const verifyifTeacher = async (req:Request, res:Response, next:NextFunction):Promise<void> => {
+    try{
+    const token:string = req.cookies.token
+    const decoded = jwt.verify(token, process.env.SECRET!) as TokenPayloadDTO
+   
+   decoded.isTeacher ? next():res.status(401).json({err:true,message:`you are not a teacher`})
+   
+    }catch(err){
+        res.status(401).json({err:true,message:`${err}`})
+    }
+}
+
+export const verifyifStudent = async (req:Request, res:Response, next:NextFunction):Promise<void> => {
     try{
     const token:string = req.cookies.token
     const decoded = jwt.verify(token, process.env.SECRET!) as TokenPayloadDTO
@@ -14,5 +26,3 @@ const verify = async (req:Request, res:Response, next:NextFunction):Promise<void
         res.status(401).json({err:true,message:`${err}`})
     }
 }
-
-export default verify
